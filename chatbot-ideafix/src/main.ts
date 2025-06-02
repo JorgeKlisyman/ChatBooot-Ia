72d6339857f3b835ea473d874c889fd55bceb2d1
JorgeKlisyman <132587757+JorgeKlisyman@users.noreply.github.com>
2025-06-02 00:17:53 -0300

    Iniciando o projeto

--- chatbot-ideafix/src/main.ts
+++
@@ -1,5 +0,0 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { ChatComponent } from './app/chat/chat.component';
-
-bootstrapApplication(ChatComponent)
-  .catch(err => console.error(err));
